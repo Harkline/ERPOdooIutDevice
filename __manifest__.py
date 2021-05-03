1392134c9e1a4af89988a7f34119a3b15ddbedb5
yanis levesque <yanislevesque@hotmail.fr>
2021-05-03 17:27:29 +0200

    Ajout du menu secondaire : Brand

--- __manifest__.py
+++ __manifest__.py
@@ -29,7 +29,7 @@
         # 'security/ir.model.access.csv',
         'views/iut_it_device_view.xml',
 
-        # 'views/iut_it_brand_view.xml',
+        'views/iut_it_brand_view.xml',
         'views/arborescence_menu_view.xml',
         # 'views/iut_it_model_view.xml',
         # 'views/iut_model_type_view.xml',
--- models/iut_it_brand.py
+++ models/iut_it_brand.py
@@ -6,9 +6,9 @@ class IutItBrand(models.Model):
 
     _python_sql_constraints_Unique_Key = [('name_uniq', 'unique (name)', 'name is a unique key, violation of constraint !')]
 
-    name = fields.Char(required=True)
-    warrantyDelayMonth = fields.Integer(string="warrantyDelayMonth")
-    supportPhone = fields.Integer(string="supportPhone")
+    name = fields.Char(string="Name", required=True)
+    warrantyDelayMonth = fields.Integer(string="Warranty delay Month")
+    supportPhone = fields.Integer(string="Phone support")
 
     #Permet de faire le lien BD 1...*              ids
     modelId = fields.One2many('iut.it.model', 'brandId');
